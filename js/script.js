004b1cffd91492a31641ba9f6e3b558f709be797
Greta Grigelionyte <greta.grigelionyte@gmail.com>
2017-10-29 18:41:15 +0200

    update1

--- js/script.js
+++ js/script.js
@@ -3,6 +3,60 @@ var lastScrollTop = 0;
 var delta = 5;
 var navbarHeight = $('.top').outerHeight();
 
+$(document).ready(function(){
+  var open = false;
+  Moveit.put(first, {
+    start: '0%',
+    end: '14%'
+  });
+  Moveit.put(middle, {
+    start: '0%',
+    end: '100%'
+  });
+  Moveit.put(second, {
+    start: '0%',
+    end: '11.5%'
+  });
+  $('.trigger').click(function(){
+    if(!open){
+    Moveit.animate(first, {
+      start: '78%',
+      end: '93%',
+      duration: 1
+    });
+    Moveit.animate(middle, {
+      start: '50%',
+      end: '50%',
+      duration: 1
+    });
+    Moveit.animate(second, {
+      start: '81.5%',
+      end: '94%',
+      duration: 1
+    });
+    }
+
+    else {
+      Moveit.animate(first, {
+      start: '0%',
+      end: '14%',
+      duration: 1
+    });
+    Moveit.animate(middle, {
+      start: '0%',
+      end: '100%',
+      duration: 1
+    });
+    Moveit.animate(second, {
+      start: '0%',
+      end: '11.5%',
+      duration: 1
+    });
+    }
+    open = !open;
+  })
+})
+
 $(window).scroll(function(event){
     didScroll = true;
 });
